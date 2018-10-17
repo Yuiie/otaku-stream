da10043b42a168b06c8e8868c1d3c26354c041e7
sinamal <morgan.sinamal@epitech.eu>
2018-10-17 13:02:02 +0200

    add categorie

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategorieRepository")
+ */
+class Categorie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Anime", mappedBy="categorie")
+     */
+    private $animes;
+
+    public function __construct()
+    {
+        $this->animes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Anime[]
+     */
+    public function getAnimes(): Collection
+    {
+        return $this->animes;
+    }
+
+    public function addAnime(Anime $anime): self
+    {
+        if (!$this->animes->contains($anime)) {
+            $this->animes[] = $anime;
+            $anime->addCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnime(Anime $anime): self
+    {
+        if ($this->animes->contains($anime)) {
+            $this->animes->removeElement($anime);
+            $anime->removeCategorie($this);
+        }
+
+        return $this;
+    }
+}
